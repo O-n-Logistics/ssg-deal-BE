a2b72c50d0768a894c9760876bca623359918454
hyper <revoluer9@gmail.com>
2025-04-12 22:54:25 +0900

    feat: OrderService UserServiceFeignClient URL 엔드포인트 변경

--- order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/user/feign/UserServiceFeignClient.java
+++ order_service/src/main/java/on/ssgdeal/order_service/infrastructure/client/user/feign/UserServiceFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(name = "user-service")
 public interface UserServiceFeignClient {
 
-    @PostMapping("/internal/v1/users/validations/deliveries")
+    @PostMapping("/internal/v1/users/destinations/validations")
     CommonResponse<ValidDestinationResponseDto> validDestinationRequest(
         @RequestBody ValidDestinationRequestDto requestDto);
 
