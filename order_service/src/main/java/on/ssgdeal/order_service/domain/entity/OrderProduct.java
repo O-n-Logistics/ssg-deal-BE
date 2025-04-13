e07a3dc54d18b36490fddd7f5bf1e7ae0e2ae777
hyper <revoluer9@gmail.com>
2025-04-13 19:06:23 +0900

    fix: OrderProduct BaseEntity Extends

--- order_service/src/main/java/on/ssgdeal/order_service/domain/entity/OrderProduct.java
+++ order_service/src/main/java/on/ssgdeal/order_service/domain/entity/OrderProduct.java
@@ -14,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import on.ssgdeal.common.jpa.BaseEntity;
 import on.ssgdeal.order_service.domain.entity.dtos.CreateOrderProductDto;
 import on.ssgdeal.order_service.domain.vo.Quantity;
 import org.hibernate.annotations.SQLRestriction;
@@ -24,7 +25,7 @@ import org.hibernate.annotations.SQLRestriction;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @SQLRestriction("is_deleted = false")
-public class OrderProduct {
+public class OrderProduct extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
