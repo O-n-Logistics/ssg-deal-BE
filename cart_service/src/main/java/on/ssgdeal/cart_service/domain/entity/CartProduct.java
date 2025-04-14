7d7b769b340e8df52ec36f28f580be5a55a628da
jeonjuho <wjswngh23@daum.net>
2025-04-14 17:05:34 +0900

    refactor(ProductQuantity): 상품 수량을 VO 객체로 변경
    - 상품 수량의 검증을 VO 객체를 사용하여 역할을 분리하도록 리팩터링

--- cart_service/src/main/java/on/ssgdeal/cart_service/domain/entity/CartProduct.java
+++ cart_service/src/main/java/on/ssgdeal/cart_service/domain/entity/CartProduct.java
@@ -5,24 +5,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import on.ssgdeal.cart_service.exception.CartException.MustBePositiveQuantityException;
+import on.ssgdeal.cart_service.domain.vo.ProductQuantity;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("cart_product")
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 @Builder(access = lombok.AccessLevel.PRIVATE)
-@Getter
 public class CartProduct {
 
     @Id
+    @Getter
     private String hashKey;
-    private Long quantity;
+    private ProductQuantity quantity;
 
     public static CartProduct create(String hashKey, Long quantity) {
         return CartProduct.builder()
             .hashKey(hashKey)
-            .quantity(quantity)
+            .quantity(new ProductQuantity(quantity))
             .build();
     }
 
@@ -35,9 +35,10 @@ public class CartProduct {
     }
 
     public void increaseQuantity(Long quantity) {
-        if (quantity <= 0) {
-            throw new MustBePositiveQuantityException();
-        }
-        this.quantity += quantity;
+        this.quantity.increase(quantity);
+    }
+
+    public Long getQuantity() {
+        return this.quantity.getValue();
     }
 }
